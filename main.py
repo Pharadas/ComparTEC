337bd0276e2c2fe8f078d5dea9288b80629c1a1c
Pharadas <davidsudo18@gmail.com>
2021-08-28 23:54:24 -0500

    junte las nuevas pantallas

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 # kivy imports
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.scrollview import ScrollView
 from kivy.core.window import Window
+from kivy.uix.widget import Widget
+from horarios import Horarios
+from horarios import Asesoria
 
 # screens imports
 from search import Search
@@ -23,6 +22,7 @@ Builder.load_string("""
             on_press: root.manager.current = 'search'
         Button:
             text: 'Maes'
+            on_press: root.manager.current = 'asesorias'
 
 <SearchScreen>:
     Search:
@@ -30,6 +30,34 @@ Builder.load_string("""
 		Button:
 			text: 'Back'
             on_press: root.manager.current = 'menu'
+
+<AsesoriasScreen>:
+    BoxLayout:
+        orientation: 'vertical'
+        Button:
+            text: 'Inicio'
+            size_hint: (1.0, 0.2)
+            on_press: root.manager.current = 'menu'
+        Button:
+            text: 'Consulta horarios'
+            on_press: root.manager.current = 'horarios'
+        Button:
+            text: 'Registra asesoria'
+            on_press: root.manager.current= 'asesoria'
+
+<HorariosScreen>:
+    Horarios:
+        size_hint: (1, None)
+        Button:
+            text: 'Back'
+            on_press: root.manager.current = 'asesorias'
+
+<AsesoriaScreen>:
+    Asesoria:
+        size_hint: (1, None)
+        Button:
+            text: 'Back'
+            on_press: root.manager.current = 'asesorias'
 """)
 
 # Declare both screens
@@ -39,14 +67,30 @@ class MenuScreen(Screen):
 class SearchScreen(Screen):
     pass
 
+class AsesoriasScreen(Screen):
+    pass
+
+class HorariosScreen(Screen):
+    pass
+
+class AsesoriaScreen(Screen):
+    pass
+
 class TestApp(App):
 	def build(self):
 		sm = ScreenManager()
 		sm.add_widget(MenuScreen(name='menu'))
 		sm.add_widget(SearchScreen(name='search'))
-		# sm.add_widget(Search(name='search'))
+		sm.add_widget(AsesoriasScreen(name='asesorias'))
+		sm.add_widget(HorariosScreen(name='horarios'))
+		sm.add_widget(AsesoriaScreen(name='asesoria'))
 
 		return sm
+
+def checkResize(width, height):
+	Window.size = (width, height)
+
+Window.bind(on_resize=checkResize)
 
 if __name__ == '__main__':
     TestApp().run()
--- search.py
+++ search.py
@@ -1,11 +1,10 @@
-from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.scrollview import ScrollView
 from kivy.core.window import Window
 from kivy.uix.textinput import TextInput
-from kivy.uix.screenmanager import Screen
+from kivy.uix.image import Image
 
 class Search(BoxLayout):
 	def __init__(self, **kwargs):
@@ -17,9 +16,16 @@ class Search(BoxLayout):
 		self.orientation = 'vertical'
 		layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
 		layout.bind(minimum_height=layout.setter('height'))
-		for i in range(20):
-			btn = Button(text=str(i), size_hint_y=None, height=40)
-			layout.add_widget(btn)
+		# for i in range(20):
+		# 	# buttonBox = BoxLayout(orientation='vertical', size=(Window.width, 500))
+		# 	# buttonBox.add_widget(Button(text=str(i), size_hint_y=None, height=40))
+		# 	# buttonBox.add_widget(Image(source='MayI.PNG'))
+		# 	# layout.add_widget(buttonBox)
+			# layout.add_widget(Button(text=str(i), size_hint_y=None, height=40))
+		layout.add_widget(Image(source='MayI.PNG'))
+			# root.add_widget(Image(source='MayI.PNG'))
+
 		root = ScrollView(size_hint=(1, None), size=(Window.width, Window.height - 115))
+		# root.add_widget(Image(source='MayI.PNG'))
 		root.add_widget(layout)
 		self.add_widget(root)
